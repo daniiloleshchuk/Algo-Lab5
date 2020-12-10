574bede3773986f2167af0e9246b478b14a2cba1
Daniil Oleshchuk <danieloleshuk@gmail.com>
2020-12-10 17:05:45 +0200

    added and checked given tests, everything seems to be ok

--- tests.py
+++ tests.py
@@ -3,10 +3,31 @@ from kmp import kmp
 
 
 class KMPTest(unittest.TestCase):
-    def setUp(self) -> None:
-        self.string1 = "asjfbmcvkajs,hfbvchajlhsdfcbvhhiABCfabsjhdhqahsdguhABueflahsguoilhACailshdfgABDaljhfsgABC"
-        self.pattern1 = "ABC"
 
     def test_first_case(self):
-        matches = kmp(self.string1, self.pattern1)
-        self.assertEqual(matches, [(32, 34), (86, 88)])
+        matches = kmp("aaa", "aa")
+        self.assertEqual(
+            [(0, 1), (1, 2)],
+            matches
+        )
+
+    def test_second_case(self):
+        matches = kmp("bbabbb", "bb")
+        self.assertEqual(
+            [(0, 1), (3, 4), (4, 5)],
+            matches
+        )
+
+    def test_third_case(self):
+        matches = kmp("bbbabbbb", "bbb")
+        self.assertEqual(
+            [(0, 2), (4, 6), (5, 7)],
+            matches
+        )
+
+    def test_fourth_case(self):
+        matches = kmp("wassdvsasdfwasdcwsadwadsvseawasdfvbdfs", "wasd")
+        self.assertEqual(
+            [(11, 14), (28, 31)],
+            matches
+        )
